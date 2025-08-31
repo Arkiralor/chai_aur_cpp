b68aa19044b245202e38814b85864c4756ecf37e
Prithoo Medhi <prithoo11335@gmail.com>
2025-08-31 15:37:25 +0530

    primitive datatypes

---
+++ src/002_primitives/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int teaLeafCount = 50;
+    float waterTemperature = 98;
+    double rateOfOneCupTea = 127.60;
+    char teaGrade = 'A';
+    bool teaIsReady = true;
+
+    std::cout<<"Tea Leaf Count: "<<teaLeafCount<<std::endl;
+    std::cout<<"Water Temperature: "<<waterTemperature<<std::endl;
+    std::cout<<"Rate of One Cup Tea: "<<rateOfOneCupTea<<std::endl;
+    std::cout<<"Tea Grade: "<<teaGrade<<std::endl;
+    std::cout<<"Is Tea Ready: "<<teaIsReady<<std::endl;
+    return 0;
+}
